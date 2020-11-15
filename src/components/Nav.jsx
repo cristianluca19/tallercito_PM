6444db4f76f9c44312929c0faaa0cdae69d147f4
cristianluca19 <cristianlucatti@gmail.com>
2020-11-15 12:07:18 -0300

    domingo de tallercito

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import Logo from '../assets/rick-and-morty-logo.png'
 
 export default function Nav() {
 
     return (
         <div>
             <nav className="navbar navbar-dark bg-dark">
+                <img src={Logo} alt='' width={180} />
                 <a className="navbar-brand" href='/'>Tallercito PM's</a>
                 {/* <form className="form-inline">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
